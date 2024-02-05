a29ab26d58e24a0ca84e58ee1ae9f39eb0b8a996
kuznetsovanatalia <FCiZtjXe88!@>
2024-02-06 00:22:00 +0100

    autorization service

--- src/main/java/model/Credentials.java
+++ src/main/java/model/Credentials.java
@@ -3,5 +3,5 @@ package model;
 public class Credentials {
     public final static String url = "jdbc:mysql://localhost:3306/pizza_cafe";
     public final static String uName = "root";
-    public final static String password = "16031995Lavrov";
+    public final static String password = "16031995Lavrov!";
 }
